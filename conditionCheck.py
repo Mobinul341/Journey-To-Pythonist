3f175bb4b0c18566502d4833b322299d0cbd0ceb
Mobinul Kawshik <engr.kawshik@ymail.com>
2019-09-30 22:17:47 +0600

    If-elif-else condition check

---
+++ conditionCheck.py
@@ -0,0 +1,28 @@
+#car = input("Enter a Car name: ")
+'''
+#if-else statement check 
+if car == "BMW":
+    print("I wish to Buy a Black BMW")
+
+else:
+
+    print("Go buy a Nano")
+'''   
+'''
+#if-elif-else statement check
+
+if car == "Toyota":
+
+    print("I can buy this car")
+elif car == "Nano":
+    print("Thios is a cheap indian car")
+
+elif car == "Tata":
+    print("It makes Truck")
+
+
+else:
+    print("I do not have any car")
+'''
+
+    
