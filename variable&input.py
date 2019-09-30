b2434a6e81515621c0854ac357b2549ed2876a77
Mobinul Kawshik <engr.kawshik@ymail.com>
2019-09-30 22:16:58 +0600

    Variables, Data Type and User Input

---
+++ variable&input.py
@@ -0,0 +1,23 @@
+'''
+#User Input by input()
+newVar = input()
+#Printing the output with Type ()
+print(newVar, "\n", type(newVar))'''
+
+'''
+#Integer input with int()
+newVar1 = int(input("Enter a Number: "))
+print(newVar1, "\n", type(newVar1))
+'''
+'''
+#Float input with float()
+newVar2 = float(input("Enter a Float number: "))
+print(newVar2, "\n", type(newVar2))
+'''
+
+'''
+#Operation of title(), upper(), lower()
+newSentence = input("Enter a Sentence")
+
+print(newSentence.title(), "\n", newSentence.upper(), "\n",newSentence.lower())
+'''
