23cc486389224f51cc2a69527dc3e2d844cea0cd
Mobinul Kawshik <engr.kawshik@ymail.com>
2019-10-02 22:24:33 +0600

    Python basic loop structure

---
+++ loopTest.py
@@ -0,0 +1,58 @@
+#a = 1
+'''
+#While loop basic structure 
+while a<=10:
+    print("Number: ", a)
+    a+=1
+'''
+
+
+#x = int(input("Enter a number to set range: "))
+
+'''
+#While loop 
+while a<=x:
+    print("Show",a)
+    a+=1
+'''
+
+'''
+#For loop basic
+for n in range(5, 16):
+    print("Number", n)
+ '''
+'''
+#For loop keyboard input 
+for a in range(x):
+    print('Nums', a)
+'''
+
+'''
+#Break case
+
+for a in range(10):
+    if a==5:
+        break
+    print(a)
+'''
+'''
+#Continue case
+
+for z in range(8):
+
+
+    if z== 5:
+        continue
+
+    print("hi", z)
+'''
+'''
+#Pass case
+
+for e in range(10):
+    if e == 6:
+        pass 
+    print(e)
+'''
+
+    
