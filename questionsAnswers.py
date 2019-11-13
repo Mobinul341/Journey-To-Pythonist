44aa1863eb76812c736b6abcbda7060427fac22d
Mobinul Kawshik <engr.kawshik@ymail.com>
2019-11-13 22:38:43 +0600

    Solution of Problems

---
+++ questionsAnswers.py
@@ -0,0 +1,41 @@
+#problem 1
+'''
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+print(" Addition: ", num1+num2,'\n', "Subtraction: ", num1-num2,'\n', "Multiplication: ", num1*num2, '\n', "Division: ", num1/num2,'\n')
+'''
+#problem 2
+'''
+height = [6.08, 12.09, 15.05, 25.07, 75.11]
+summation = sum(height)
+print("Average: ", summation /5)
+'''
+
+#problem 3
+'''
+for nums in range(5):
+   firstNum = int(input("Enter 1: "))
+   secondNum = int(input("Enter 2: "))
+
+   if firstNum > secondNum:
+        print(firstNum, "is Greater")
+   elif firstNum == secondNum:
+        break
+   else:
+        print(secondNum, "is Greater")
+'''
+
+#problem 4
+'''
+foods = ['Burger', 'Sandwich', 'Pizza']
+
+foods.append('Candy')
+foods.append('Coca Cola')
+
+foods.remove('Pizza')
+print(foods)
+'''
+
+
+    
