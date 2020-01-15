b37166a19e6378ca82b8a3397b8963051bbc5141
Mobinul Kawshik <engr.kawshik@ymail.com>
2020-01-15 22:38:30 +0600

    Function basic syntax

---
+++ funcLearning.py
@@ -0,0 +1,21 @@
+
+#Without Parameter basic function syntax
+def kptech_pyway():
+    print("Hello KPtech")
+
+kptech_pyway()
+
+
+#with 1 parameter and 1 argument basic function syntax
+studentID = input("Enter a Name: ")
+def kpetchs(student):
+    print("Hello %s"%student)
+
+kpetchs(studentID)
+
+
+#multiple parameter and arguments in a function basic syntax
+def number_sum(x,y):
+    print("%d / %d = %.3f"%(x, y, x/y))
+
+number_sum(13, 37)
