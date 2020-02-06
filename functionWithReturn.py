4f32c330216dce45d2ff94d5cf6317e4aa3f4cb8
Mobinul Kawshik <engr.kawshik@ymail.com>
2020-02-06 09:44:33 +0600

    Function with return values

---
+++ functionWithReturn.py
@@ -0,0 +1,35 @@
+'''def food(fruit, drink):
+    print("These foods are good: %s, %s"%(fruit, drink))
+
+food('apple', 'milk')
+
+food( drink='Tea', fruit = 'Banana')
+
+
+def biriyani(rice, meat='Mutton'):
+    print("My favorite Biriyani is cooked with %s rice and %s meat"%(rice, meat))
+
+biriyani(rice = 'Basmati', meat = 'Chicken')
+'''
+
+def addition(num1, num2):
+    return num1 + num2
+def multiplicat(num1, num2):
+    return num1 * num2
+
+print("Result is: ",addition(num1 = 5, num2 = 8),"and Multiply is: ", multiplicat(num1 = 9,num2 = 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
