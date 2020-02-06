5677a95f5c523d2fdca363d72f757f1ad5a821ca
Mobinul Kawshik <engr.kawshik@ymail.com>
2020-02-06 09:42:59 +0600

    Function positional argument

---
+++ funcPOstion.py
@@ -0,0 +1,16 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+def calculation(first_num, second_num, third_num):
+    print("Calculation is (%d + %d)**%d = %d"%(first_num, second_num, third_num, (first_num+second_num)**third_num))
+first_num = num1
+second_num = num2
+third_num = num3
+
+
+
+calculation(num1, num2, num3)
+
+
+
