5b09ffe88a602773609af926eded0f4fde83ffb7
Mobinul Kawshik <engr.kawshik@ymail.com>
2020-02-12 23:20:35 +0600

    Import from Maths file
    This program is about learning import file as module in calc.py file and use the functions of maths.py file.

---
+++ calc.py
@@ -0,0 +1,57 @@
+
+'''
+import maths as mt 
+numX = float(input("Enter a number: "))
+numY = float(input("Enter another number: "))
+
+print("%.4f"%(mt.division(numX, numY)))
+
+'''
+
+
+
+'''
+import maths.addition as ad 
+
+print(ad(94,6))
+'''
+
+
+
+
+
+'''
+from maths import *
+print(subtrct(5,9))
+print(addition(47,3))
+print(division(15,2))
+print(roots(21))
+print("%.6f"%pi)
+'''
+
+
+
+
+'''
+import maths as mt 
+print(mt.roots(9))
+'''
+
+
+
+
+
+'''
+from maths import addition as ad 
+print(ad(15,20))
+'''
+
+
+'''
+
+from maths import pi
+
+print("%.2f"%pi)
+print(roots(pi))
+'''
+
---
+++ maths.py
@@ -0,0 +1,14 @@
+def addition(num1, num2):
+    return num1 + num2
+
+def subtrct(num1, num2):
+    return num1 - num2
+
+def division(num1,num2):
+    return num1/num2
+
+def roots(num1):
+    return float(num1)**0.5
+
+pi = 3.14159265359
+
